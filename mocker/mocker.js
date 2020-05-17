543ff97dc6f5aa7d50819027ce0f9bfdce0040c6
zhangqing <heyzqt@qq.com>
2020-05-17 15:10:16 +0800

    添加城市搜索接口

--- mocker/mocker.js
+++ mocker/mocker.js
@@ -10,5 +10,25 @@ module.exports = {
       status: 200,
       data: "hello heyzqt"
     });
+  },
+  "GET /rest/search"(req, res) {
+    const { key } = req.query;
+    return res.json({
+      result: [
+        {
+          key: "芜湖",
+          display: "芜湖"
+        },
+        {
+          key: "井冈山",
+          display: "井冈山"
+        },
+        {
+          key: "铁岭",
+          display: "铁岭"
+        }
+      ],
+      searchKey: key
+    });
   }
 };
